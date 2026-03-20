c7a99b59f0f5c3217ecf789e14a18423823f29c7
PixelCraft Developer <pixelcraft@example.com>
2026-03-21 00:39:58 +0530

    Fix: connect-mongo default import to resolve MongoStore.create crash

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then(() => console.log('MongoDB Connected ✅'))
   .catch(err => console.error('MongoDB Error:', err));
 
-const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo').default || require('connect-mongo').MongoStore;
 
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ limit: '20mb', extended: true }));
